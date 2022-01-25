376fecf9a1bbeee1675d761c9dbb4962cf4ae51a
PriscillaThung <97488594+PriscillaThung@users.noreply.github.com>
2022-01-25 09:57:48 +0800

    Add files via upload

---
+++ test.java
@@ -0,0 +1,37 @@
+public class test {
+    
+    public static void main(String[] args) throws Exception {
+        /*
+        System.out.println("Hello, I am Priscilla");
+
+        String moduleCode = "CSC1009";
+        switch(moduleCode)
+        {
+            case "CSC1006":
+            System.out.println("Mathematics 2");
+            break;
+            case "CSC1007":
+            System.out.println("Operating Systems");
+            break;
+            case "CSC1008":
+            System.out.println("Data Structure and Algorithms");
+            break;
+            case "CSC1009":
+            System.out.println("Object Orientated Programming");
+            break;
+            case "CSC1010":
+            System.out.println("Computer Network");
+            break;
+        }
+        */
+
+        for(int x = 102; x >= 66; x--)
+        {
+            if(x%2 == 1)
+            {
+                System.out.println("value of x: " + x);  
+            }
+        }
+        
+    }   
+}
